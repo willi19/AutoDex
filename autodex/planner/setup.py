789599967331adfa568cdac1536f52681945c3fe
willi19 <willi19@snu.ac.kr>
2026-03-22 14:11:23 +0900

    Fix setup.py to write .so into src/curobo/curobolib/

--- autodex/planner/setup.py
+++ autodex/planner/setup.py
@@ -18,6 +18,7 @@
 # * https://setuptools.pypa.io/en/latest/setuptools.html#setup-cfg-only-projects
 
 # Standard Library
+import os
 import sys
 
 # Third Party
@@ -85,9 +86,17 @@ ext_modules = [
     ),
 ]
 
+class BuildExtInplace(BuildExtension):
+    """Force .so files into src/curobo/curobolib/ instead of curobo/curobolib/."""
+    def get_ext_fullpath(self, ext_name):
+        path = super().get_ext_fullpath(ext_name)
+        if not path.startswith("src/"):
+            path = os.path.join("src", path)
+        return path
+
 setuptools.setup(
     ext_modules=ext_modules,
-    cmdclass={"build_ext": BuildExtension},
+    cmdclass={"build_ext": BuildExtInplace},
     package_data={"": ["*.so"]},
     include_package_data=True,
 )
